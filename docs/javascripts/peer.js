235b9c78337da1a11b75daff71a8b90a2bcc1418
Patrick Griffin <58729+firien@users.noreply.github.com>
2020-04-11 23:37:35 -0400

    share links

--- docs/javascripts/peer.js
+++ docs/javascripts/peer.js
@@ -6,97 +6,134 @@ const configuration = { iceServers: [{
 };
 
 const peerConnection = new RTCPeerConnection(configuration);
+
+window.addEventListener('storage', () => {
+  if (peerConnection.remoteDescription == null) {
+    let description = localStorage.getItem('answer');
+    if (description) {
+      let offer = new RTCSessionDescription(JSON.parse(atob(description)));
+      peerConnection.setRemoteDescription(offer);
+      localStorage.removeItem('answer');
+    }
+  }
+});
+
+
+const noop = (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+}
 let dataChannel;
 
 peerConnection.onicecandidate = (e) => {
   if (e.candidate) return;
+  let param = (peerConnection.remoteDescription == null) ? 'offer' : 'answer';
   let offer = btoa(JSON.stringify(peerConnection.localDescription));
+  let url = new URL(`${location.protocol}//${location.host}${location.pathname}`)
+  url.searchParams.set(param, offer)
   let link = document.createElement('a');
   link.href = '#';
-  link.textContent = 'Token'
+  link.textContent = 'Share Link';
   link.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (navigator.share) {
+    noop(e);
+    if (navigator.shared) {
       navigator.share({
         title: link.textContent,
-        text: offer,
+        // text: 'offer',
+        url: url.toString(),
       });
     } else {
-      navigator.clipboard.writeText(offer);
+      navigator.clipboard.writeText(url);
     }
   })
   document.body.appendChild(link);
 }
 peerConnection.oniceconnectionstatechange = (e) => {
   let state = peerConnection.iceConnectionState;
-  console.log(state);
+  // console.log(state);
+  if (state === 'connected') {
+    // let span = document.createElement('span');
+    // span.textContent = 'Connection Made';
+    // document.body.appendChild(span);
+  }
 };
-
+const onMessage = (e) => {
+  let span = document.createElement('span');
+  span.classList.add('response')
+  span.textContent = e.data;
+  let list = document.querySelector('#messages')
+  list.appendChild(span);
+}
 const createOfferSDP = async () => {
-  let formResponse = document.querySelector('form#response');
-  formResponse.classList.remove('hidden');
+  // let formResponse = document.querySelector('form#response');
+  // formResponse.classList.remove('hidden');
   dataChannel = peerConnection.createDataChannel("chat");
   let sessionDescription = await peerConnection.createOffer();
   peerConnection.setLocalDescription(sessionDescription);
   dataChannel.onopen = (e) => {
-    console.log("opened")
+    let span = document.createElement('span');
+    span.textContent = 'Connection Made';
+    document.body.appendChild(span);
   }
-  dataChannel.onmessage = (e) => {
-    let div = document.createElement('div');
-    div.classList.add('response')
-    div.textContent = e.data;
-    document.body.appendChild(div);
-  }
+  dataChannel.onmessage = onMessage;
 };
 
 peerConnection.ondatachannel = (e) => {
   dataChannel = e.channel;
   dataChannel.onopen = (e) => {
-    console.log("opened")
+    let span = document.createElement('span');
+    span.textContent = 'Connection Made';
+    document.body.appendChild(span);
   }
-  dataChannel.onmessage = (e) => {
-    let div = document.createElement('div');
-    div.classList.add('response')
-    div.textContent = e.data;
-    document.body.appendChild(div);
-  }
+  dataChannel.onmessage = onMessage;
 }
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', async (e) => {
   let button = document.querySelector('#create');
   button.addEventListener('click', createOfferSDP);
 
-  let joinButton = document.querySelector('#join');
-  joinButton.addEventListener('click', (e) => {
-    let formAnswer = document.querySelector('form#answer');
-    formAnswer.classList.remove('hidden');
-  })
-  let formAnswer = document.querySelector('form#answer');
-  formAnswer.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    let offer = new RTCSessionDescription(JSON.parse(atob(formAnswer.data.value)));
+  // was it loaded with offer?
+  let url = new URL(window.location);
+  if (url.searchParams.has('offer')) {
+    button.classList.add('hidden');
+    let paramOffer = url.searchParams.get('offer');
+    let offer = new RTCSessionDescription(JSON.parse(atob(paramOffer)));
     peerConnection.setRemoteDescription(offer);
     let sessionDescription = await peerConnection.createAnswer(sdpConstraints)
     peerConnection.setLocalDescription(sessionDescription);
-  })
+  } else if (url.searchParams.has('answer')) {
+    localStorage.setItem('answer', url.searchParams.get('answer'));
+  }
 
-  let formResponse = document.querySelector('form#response');
-  formResponse.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    e.stopPropagation();  
-    let offer = new RTCSessionDescription(JSON.parse(atob(formResponse.data.value)));
-    peerConnection.setRemoteDescription(offer);
-  })
+  // let joinButton = document.querySelector('#join');
+  // joinButton.addEventListener('click', (e) => {
+  //   let formAnswer = document.querySelector('form#answer');
+  //   formAnswer.classList.remove('hidden');
+  // })
+  // let formAnswer = document.querySelector('form#answer');
+  // formAnswer.addEventListener('submit', async (e) => {
+  //   noop(e);
+  //   let offer = new RTCSessionDescription(JSON.parse(atob(formAnswer.data.value)));
+  //   peerConnection.setRemoteDescription(offer);
+  //   let sessionDescription = await peerConnection.createAnswer(sdpConstraints)
+  //   peerConnection.setLocalDescription(sessionDescription);
+  // })
+
+  // let formResponse = document.querySelector('form#response');
+  // formResponse.addEventListener('submit', async (e) => {
+  //   noop(e);
+  //   let offer = new RTCSessionDescription(JSON.parse(atob(formResponse.data.value)));
+  //   peerConnection.setRemoteDescription(offer);
+  // })
   let formChat = document.querySelector('form#chat');
   formChat.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    e.stopPropagation();  
+    noop(e);
     dataChannel.send(formChat.message.value);
-    let div = document.createElement('div');
-    div.textContent = formChat.message.value;
-    document.body.appendChild(div);
+    let span = document.createElement('span');
+    span.classList.add('sent');
+    span.textContent = formChat.message.value;
+    let list = document.querySelector('#messages')
+    list.appendChild(span);
     formChat.reset();
   })
 })

f76efea178557d6d07c33762675a4a0012f80016
Patrick Griffin <58729+firien@users.noreply.github.com>
2020-04-22 19:58:34 -0400

    service worker not loaded yet

---
+++ docs/service.js
@@ -0,0 +1,56 @@
+const tag = 1;
+const prefix = 'DICEBB';
+const cacheName = `${prefix}-${tag}`
+
+const urls = [
+  '/dice-baseball/javascripts/die.js',
+  '/dice-baseball/javascripts/game.js',
+  '/dice-baseball/javascripts/inning.js',
+  '/dice-baseball/javascripts/organ.js',
+  '/dice-baseball/javascripts/peer.js',
+  '/dice-baseball/javascripts/player.js',
+  '/dice-baseball/javascripts/team.js',
+  '/dice-baseball/javascripts/utils.js',
+  '/dice-baseball/stylesheets/index.css',
+  '/dice-baseball/images/icon-152.png',
+  '/dice-baseball/images/icon-167.png',
+  '/dice-baseball/images/icon-180.png',
+  '/dice-baseball/images/icon-192.png',
+  '/dice-baseball/images/icon-512.png',
+  '/dice-baseball/index.html',
+  '/dice-baseball/',
+]
+
+self.addEventListener('install', (event) => {
+  return event.waitUntil(caches.open(cacheName).then((cache) => {
+    return cache.addAll(urls)
+  }))
+})
+
+const clearPreviousCaches = () => {
+  return caches.keys().then((keys) => {
+    return Promise.all(keys.filter((key) => {
+      return (key != cacheName) && key.startsWith(prefix)
+    }).map(key => caches.delete(key)))
+  })
+}
+
+self.addEventListener('activate', (event) => {
+  return event.waitUntil(clearPreviousCaches())
+})
+
+self.addEventListener('fetch', (event) => {
+  return event.respondWith(
+    caches.open(cacheName).then((cache) => {
+      return cache.match(event.request, {ignoreSearch: true})
+    }).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    return self.skipWaiting()
+  }
+})

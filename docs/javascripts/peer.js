2b36516ac5ed658bc534b3fcbf13ee3218b69a6f
Patrick Griffin <58729+firien@users.noreply.github.com>
2020-04-26 00:00:42 -0400

    extract out of peer

--- docs/javascripts/peer.js
+++ docs/javascripts/peer.js
@@ -1,7 +1,4 @@
 import {noop, chunkParam, unChunkParam, logger, empty} from './utils.js'
-import Game from './game.js';
-import Die from './die.js';
-import Organ from './organ.js';
 
 const sdpConstraints = { optional: [{RtpDataChannels: true}] };
 const configuration = { iceServers: [{
@@ -113,50 +110,6 @@ peerConnection.ondatachannel = (e) => {
   dataChannel.onmessage = onMessage;
 }
 
-const updateScoreboard = (game) => {
-  let homeRuns = document.querySelector('#home-runs');
-  homeRuns.textContent = game.homeTeam.totalRuns;
-  let awayRuns = document.querySelector('#away-runs');
-  awayRuns.textContent = game.awayTeam.totalRuns;
-  //hits
-  let homeHits = document.querySelector('#home-hits');
-  homeHits.textContent = game.homeTeam.totalHits;
-  let awayHits = document.querySelector('#away-hits');
-  awayHits.textContent = game.awayTeam.totalHits;
-  //
-  let top = game.topOfInning;
-  document.querySelector('#home-team').classList.toggle('atbat', !top)
-  document.querySelector('#away-team').classList.toggle('atbat', top)
-  //
-  let t = game.topOfInning ? 'a' : 'h';
-  let id = `#${t}${game.inningNumber}`
-  if (game.currentInning) {
-    document.querySelector(id).textContent = game.currentInning.runs;
-  }
-}
-
-const updateBatter = (game) => {
-  let team = game.currentTeam;
-  let batter = team.currentBatter;
-  document.querySelector('#batter #name').textContent = batter.name;
-  document.querySelector('#batter #stat').textContent = batter.stats;
-  document.querySelector('#batter #bats').textContent = batter.atBats.join(", ");
-}
-window.addEventListener('inningChange', () => {
-  let keyFrame = {
-    transform: ["scale(1)", "scale(3)", "scale(1)"],
-  }
-  let options = {
-    easing: "ease-in-out", duration: 2000
-  }
-  document.querySelector('#info').animate(keyFrame, options)
-});
-let gameOver = false;
-window.addEventListener('gameOver', () => {
-  document.querySelector('svg').classList.add('gameover');
-  gameOver = true;
-  document.querySelector('button#roll').textContent = 'New Game?'
-})
 document.addEventListener('DOMContentLoaded', async (e) => {
   let button = document.querySelector('#create');
   button.addEventListener('click', createOfferSDP);
@@ -194,54 +147,4 @@ document.addEventListener('DOMContentLoaded', async (e) => {
     }
   })
   
-  let game = new Game();
-  let roller = document.querySelector('button#roll');
-  let div = document.querySelector('div#roll-result');
-  roller.addEventListener('click', (e) => {
-    if (gameOver) {
-      window.location.reload();
-      return;
-    }
-    roller.disabled = true;
-    let roll = game.roll();
-    roll.forEach((number, i) => {
-      let text = String.fromCharCode(Die.faces[number]);
-      let span = div.querySelector(`.die:nth-child(${i+1})`);
-      span.textContent = text;
-    })
-    let outcome = game.bat(roll);
-    document.querySelector('#outcome').textContent = outcome;
-    // bug: on safari env() is not available in calc
-    // let calc = "calc(-100% - env(safe-area-inset-bottom))";
-    let bottom = getComputedStyle(roller).bottom;
-    let calc = `calc(-100% - (${bottom} * 2))`;
-    calc = `-100%`;
-    let animation = div.animate({
-        transform: ["translateY(0)", `translateY(${calc})`, `translateY(${calc})`, "translateY(0)"],
-        offset: [0, 0.2, 0.8, 1]
-      },
-      { easing: "ease-in-out", fill: 'forwards', duration: 2000
-    });
-    animation.onfinish = () => {
-      roller.disabled = false;
-      //update batter
-      updateBatter(game);
-    };
-    //update field
-    for (let base=1; base<=3; base++) {
-      let player = game.currentTeam.playerOn(base);
-      let rect = document.querySelector(`rect[data-base='${base}']`)
-      rect.classList.toggle('occupied', !!player);
-      let span = document.querySelector(`#runners span:nth-child(${base})`)
-      span.textContent = player ? player.name : '';
-    }
-    //update scoreboard
-    updateScoreboard(game);
-    try {
-      document.querySelector('#info').textContent = game.inningTitle;
-    } catch (e) {
-      //game is probably over
-    }
-  })
-  document.querySelector('svg').addEventListener('click', Organ.ballGame);
 })
